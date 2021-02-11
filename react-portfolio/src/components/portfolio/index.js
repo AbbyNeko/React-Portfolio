37f2dab6af1df60f1c22cf8992a633af352ffadd
Abigail Limpioso <alimpioso@gmail.com>
2021-02-11 10:00:59 -0500

    started working on portfolio component

--- react-portfolio/src/components/portfolio/index.js
+++ react-portfolio/src/components/portfolio/index.js
@@ -4,3 +4,21 @@
  *  categories Web Development, React, Python, and Graphic Design
  * 
  *************************************************************/
+
+ import React from 'react';
+ import './css/index.css';
+
+ class Portfolio extends React.Component{
+
+      render(props) {
+
+            return (
+
+                <div className="portfolio-section">
+                </div>
+
+            );
+
+      }
+
+ }
